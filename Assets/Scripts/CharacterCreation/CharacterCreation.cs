a652bcd7145f3778bc46453a75e74ab11dd01891
kenz <kenz@hboxs.com>
2018-06-28 18:05:09 +0800

    first commit

---
+++ Assets/Scripts/CharacterCreation/CharacterCreation.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCreation : MonoBehaviour 
+{
+    public GameObject[] characterPrefabs;
+    public UIInput nameInput;
+    private GameObject[] characterGameObjects;
+    private int characterCounts;
+    private int SelectedIndex = 0;
+
+    void Start()
+    {
+        characterCounts = characterPrefabs.Length;
+        characterGameObjects = new GameObject[characterCounts];
+        for (int i = 0; i < characterCounts; i++)
+        {
+            characterGameObjects[i] = GameObject.Instantiate(characterPrefabs[i], transform.position, transform.rotation) as GameObject;
+        }
+
+        UpdateShowCharacter();
+    }
+
+    void UpdateShowCharacter()
+    {
+        characterGameObjects[SelectedIndex].SetActive(true);
+        for (int i = 0; i < characterCounts; i++)
+        {
+            if (i != SelectedIndex)
+            {
+                characterGameObjects[i].SetActive(false);
+            }
+        }
+    }
+
+    public void OnNextClick()
+    {
+        SelectedIndex++;
+        SelectedIndex %= characterCounts;
+        UpdateShowCharacter();
+    }
+
+    public void OnPreClick()
+    {
+        SelectedIndex--;
+        if (SelectedIndex < 0)
+        {
+            SelectedIndex = characterCounts - 1;
+        }
+        UpdateShowCharacter();    
+    }
+
+    public void OnOkClick()
+    {
+        PlayerPrefs.SetInt("SelectedCharacterIndex", SelectedIndex);
+        PlayerPrefs.SetString("Name", nameInput.value);
+    }
+}
---
+++ Assets/Scripts/Start/PressAnyKey.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressAnyKey : MonoBehaviour 
+{
+	private bool isPressAnyKey = false;
+	private GameObject buttonContainer;
+
+	void Start()
+    {
+        buttonContainer = this.gameObject.transform.parent.Find("ButtonContainer").gameObject;
+	}
+
+	void Update()
+    {
+        if (!isPressAnyKey)
+        {
+            if (Input.anyKey)
+            {
+                ShowButton();
+            }
+        }
+	}
+
+    void ShowButton()
+    {
+        this.gameObject.SetActive(false);
+        buttonContainer.SetActive(true);
+        isPressAnyKey = true;
+    }
+}
